c3d7159f4fda549cf8f17838509fa706a260bfc1
Swetha K. V <swe0523@gmail.com>
2013-11-25 10:38:36 +0530

    Added comments, made the file "neat"

--- src/com/threads/ThreadAffinityBasicUser.java
+++ src/com/threads/ThreadAffinityBasicUser.java
@@ -1,46 +1,35 @@
 package com.threads;
 
 public class ThreadAffinityBasicUser implements Runnable{
-        int[] processors;
-        
-        public ThreadAffinityBasicUser(int[] processors) {
-                this.processors = processors;
-        }
-    public void run() {
-                ThreadAffinity t = new ThreadAffinity();
-                try {
-                                t.setaffinity_process(0, this.processors.length, this.processors);
-                 System.out.println("[+] The current CPU is " + t.sched_getcpu());
-                        } catch (Exception e) {
-                                e.printStackTrace();
-                        }
-    }
+	int[] processors;
+
+	public ThreadAffinityBasicUser(int[] processors) {
+		this.processors = processors;
+	}
+	
+	public void run() {
+		ThreadAffinity t = new ThreadAffinity();
+		try {
+			Thread currThread = Thread.currentThread();
+			t.setaffinity_process(0, this.processors.length, this.processors);
+			System.out.println("[+] For thread " + currThread.getName() + " the cpu is " + t.sched_getcpu());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
    
     public static void main(String[] args) throws InterruptedException {
-           
-            /*
-* To create new thread, use
-* Thread(Runnable thread, String threadName)
-* constructor.
-*
-*/
-                    int[] p1 = {0, 1};
-                    int[] p2 = {2, 3};
-            Thread t1 = new Thread(new ThreadAffinityBasicUser(p1), "My Thread");
-            Thread t2 = new Thread(new ThreadAffinityBasicUser(p2), "My Thread");
+    	/* Specify processors */
+		int[] p1 = {0, 1};
+		int[] p2 = {2, 3};
+		
+		/* Create two threads with processors as arguments */
+		Thread t1 = new Thread(new ThreadAffinityBasicUser(p1), "My Thread One");
+		Thread t2 = new Thread(new ThreadAffinityBasicUser(p2), "My Thread Two");
 
-            /*
-* To start a particular thread, use
-* void start() method of Thread class.
-*
-* Please note that, after creation of a thread it will not start
-* running until we call start method.
-*/
-           
-            t1.start();      t1.join();
-            t2.start();
-      
-            t2.join();
-              
+		/* Start the first thread and wait for it to join */
+		t1.start(); t1.join();
+		/* Start the second thread and wait for it to join */
+		t2.start(); t2.join();         
     }
 }
